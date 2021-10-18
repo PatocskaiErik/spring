85d4716e2dd8cde4343e9f45339a6e6c3c9543ca
Erik <patoerik93@gmail.com>
2021-10-18 10:08:43 +0200

    new project

---
+++ src/main/java/flow/flowacademy/vizsga/VizsgaApplication.java
@@ -0,0 +1,13 @@
+package flow.flowacademy.vizsga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizsgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flow/flowacademy/vizsga/VizsgaApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.flowacademy.vizsga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizsgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
