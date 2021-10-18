5a28e4ad74ef2fc9a2bb13bf7d079fb39a5a9849
Erik <patoerik93@gmail.com>
2021-10-18 11:28:02 +0200

    exam

--- src/main/java/flow/flowacademy/vizsga/controllers/ErrorController.java
+++ src/main/java/flow/flowacademy/vizsga/controllers/ErrorController.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.validation.ConstraintViolationException;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
